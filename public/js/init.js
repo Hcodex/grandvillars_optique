0417e38087e8c55df8f4d13ce47e27f7b8a58def
Hcodex <61270260+Hcodex@users.noreply.github.com>
2020-06-21 13:09:43 +0200

    Email sending

--- public/js/init.js
+++ public/js/init.js
@@ -28,3 +28,36 @@ $('.navbar-nav a').click(function (event) {
     }, 500);
     event.preventDefault();
 });
+
+
+function showAlert(message, type, closeDelay) {
+
+    console.log("show alert");
+
+    var $cont = $("#alerts-container");
+
+    if ($cont.length == 0) {
+        // alerts-container does not exist, create it
+        $cont = $('<div id="alerts-container" class="msg-box">')
+            .appendTo($("body"));
+    }
+
+    // default to alert-info; other options include success, warning, danger
+    type = type || "info";
+
+    // create the alert div
+    var alert = $('<div>')
+        .addClass("fade in show alert alert-" + type)
+        .append(
+            $('<button type="button" class="close" data-dismiss="alert">')
+                .append("&times;")
+        )
+        .append(message);
+
+    // add the alert div to top of alerts-container, use append() to add to bottom
+    $cont.prepend(alert);
+
+    // if closeDelay was passed - set a timeout to close the alert
+    if (closeDelay)
+        window.setTimeout(function () { alert.alert("close") }, closeDelay);
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -13,17 +16,32 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function index(Request $request)
+    public function index(Request $request, MailerInterface $mailer)
     {
-        $contact = new Contact();
 
-        $form = $this->createForm(ContactType::class, $contact);
+        $form = $this->createForm(ContactType::class);
 
         $form->handleRequest($request);
 
-        dump($request);
-        dump($form->get('email')->getData());
-        dump($contact);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+            dump($contact);
+
+            $email = (new TemplatedEmail())
+                ->from('mailer@grandvillars-optique.fr')
+                ->replyTo($contact->getEmail())
+                ->to('contact@grandvillars-optique.fr')
+                ->subject('Message via formulaire - ' . $contact->getSubject() . ' - ' .  $contact->getLastName())
+                ->htmlTemplate('emails/contact.html.twig')
+
+                ->context([
+                    'contact' => $contact,
+                ]);
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Votre message a bien été evoyé');
+        }
 
         return $this->render('home/index.html.twig', [
             'form' => $form->createView()
