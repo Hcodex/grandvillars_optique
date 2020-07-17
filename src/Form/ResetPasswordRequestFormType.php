e78430b088208ccbee95223dcbb26730d6998dd0
Hcodex <61270260+Hcodex@users.noreply.github.com>
2020-07-17 12:02:13 +0200

    Translation of reset password

--- src/Form/ResetPasswordRequestFormType.php
+++ src/Form/ResetPasswordRequestFormType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ResetPasswordRequestFormType extends AbstractType
@@ -15,9 +16,16 @@ class ResetPasswordRequestFormType extends AbstractType
         $builder
             ->add('email', EmailType::class, [
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter your email',
+                    new Email([
+                        'message' => 'Adresse mail invalide',
                     ]),
+                    new NotBlank([
+                        'message' => 'Le champ email ne peut être vide',
+                    ]),
+
+                ],
+                'attr' => [
+                    'placeholder' => 'Votre adresse mail de connexion',
                 ],
             ])
         ;
