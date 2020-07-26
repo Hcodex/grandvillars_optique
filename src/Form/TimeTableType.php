62fb1967cc399ee521e650715393541d75bbb192
Hcodex <61270260+Hcodex@users.noreply.github.com>
2020-07-26 20:25:30 +0200

    TimeTable form display improvment

--- src/Form/TimeTableType.php
+++ src/Form/TimeTableType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\TimeTable;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,20 +13,90 @@ class TimeTableType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('monAm')
-            ->add('monPm')
-            ->add('tueAm')
-            ->add('tuePm')
-            ->add('wedAm')
-            ->add('wedPm')
-            ->add('thuAm')
-            ->add('thuPm')
-            ->add('friAm')
-            ->add('friPm')
-            ->add('satAm')
-            ->add('satPm')
-            ->add('sunAm')
-            ->add('sunPm')
+            ->add('monAm', TextType::class, [
+                'label' => 'Lundi matin',
+                'row_attr' => [
+                    'class' => 'col-6'
+                ],
+            ])
+            ->add('monPm', TextType::class, [
+                'label' => 'Lundi après midi',
+                'row_attr' => [
+                    'class' => 'col-6'
+                ],
+            ])
+            ->add('tueAm', TextType::class, [
+                'label' => 'Mardi matin',
+                'row_attr' => [
+                    'class' => 'col-6'
+                ],
+            ])
+            ->add('tuePm', TextType::class, [
+                'label' => 'Mardi après midi',
+                'row_attr' => [
+                    'class' => 'col-6'
+                ],
+            ])
+            ->add('wedAm', TextType::class, [
+                'label' => 'Mercredi matin',
+                'row_attr' => [
+                    'class' => 'col-6'
+                ],
+            ])
+            ->add('wedPm', TextType::class, [
+                'label' => 'Mercredi après midi',
+                'row_attr' => [
+                    'class' => 'col-6'
+                ],
+            ])
+            ->add('thuAm', TextType::class, [
+                'label' => 'Jeudi matin',
+                'row_attr' => [
+                    'class' => 'col-6'
+                ],
+            ])
+            ->add('thuPm', TextType::class, [
+                'label' => 'Jeudi après midi',
+                'row_attr' => [
+                    'class' => 'col-6'
+                ],
+            ])
+            ->add('friAm', TextType::class, [
+                'label' => 'Vendredi matin',
+                'row_attr' => [
+                    'class' => 'col-6'
+                ],
+            ])
+            ->add('friPm', TextType::class, [
+                'label' => 'Vendredi après midi',
+                'row_attr' => [
+                    'class' => 'col-6'
+                ],
+            ])
+            ->add('satAm', TextType::class, [
+                'label' => 'Samedi matin',
+                'row_attr' => [
+                    'class' => 'col-6'
+                ],
+            ])
+            ->add('satPm', TextType::class, [
+                'label' => 'Samedi après midi',
+                'row_attr' => [
+                    'class' => 'col-6'
+                ],
+            ])
+            ->add('sunAm', TextType::class, [
+                'label' => 'Dimanche matin',
+                'row_attr' => [
+                    'class' => 'col-6'
+                ],
+            ])
+            ->add('sunPm', TextType::class, [
+                'label' => 'Dimanche après midi',
+                'row_attr' => [
+                    'class' => 'col-6'
+                ],
+            ])
         ;
     }
 
